e130eb2350a865134f33c39b58533d31c4a9efd8
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2024-06-22 04:25:25 +0700

    bump dependencies version

--- go.mod
+++ go.mod
@@ -2,10 +2,11 @@ module github.com/mnaufalhilmym/gotracingstorageinmemory
 
 go 1.22.3
 
-require github.com/mnaufalhilmym/gotracing v0.0.13
+require github.com/mnaufalhilmym/gotracing v0.0.15
 
 require (
 	github.com/mnaufalhilmym/goasync v0.0.9 // indirect
 	github.com/mnaufalhilmym/gooption v0.0.2 // indirect
 	github.com/mnaufalhilmym/goresult v0.0.4 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 )
--- storage.go
+++ storage.go
@@ -9,10 +9,10 @@ import (
 type Storage struct {
 	sync.Mutex
 	MaxTracesPerLevel uint
-	traces            map[gotracing.Level][]gotracing.Stacktraces
+	traces            map[gotracing.Level][]gotracing.Traces
 }
 
-func (s *Storage) Insert(level gotracing.Level, trace gotracing.Stacktraces) {
+func (s *Storage) Insert(level gotracing.Level, trace gotracing.Traces) {
 	s.Lock()
 	if len(s.traces[level]) >= int(s.MaxTracesPerLevel) {
 		s.traces[level] = s.traces[level][1:]
@@ -21,13 +21,13 @@ func (s *Storage) Insert(level gotracing.Level, trace gotracing.Stacktraces) {
 	s.Unlock()
 }
 
-func (s *Storage) GetAll(level gotracing.Level) []gotracing.Stacktraces {
+func (s *Storage) GetAll(level gotracing.Level) []gotracing.Traces {
 	return s.traces[level]
 }
 
 func New(maxTracesPerLevel uint) Storage {
 	return Storage{
 		MaxTracesPerLevel: maxTracesPerLevel,
-		traces:            make(map[gotracing.Level][]gotracing.Stacktraces),
+		traces:            make(map[gotracing.Level][]gotracing.Traces),
 	}
 }
